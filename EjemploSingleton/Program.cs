da4e8eced7c673b25de9b7e3e12ebe1b6d36aa76
alexc777 <osmancruz187@gmail.com>
2022-05-21 12:44:30 -0600

    Agregar archivos de proyecto.

---
+++ EjemploSingleton/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EjemploSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciando clase para conexión a DB (se instancia una sola ven) accediendo por notación y no por new
+            SingletonDBConexion c1 = SingletonDBConexion.Conexion();
+            SingletonDBConexion c2 = SingletonDBConexion.Conexion();
+
+            // Validamos que las instancia sea en la misma
+            if (c1 == c2)
+            {
+                Console.WriteLine("Si es la misma instancia, conexión exitosa.");
+            }
+
+            Console.ReadKey();
+        }
+
+        public class SingletonDBConexion
+        {
+            static SingletonDBConexion conexion;
+            protected SingletonDBConexion()
+            {
+            }
+            public static SingletonDBConexion Conexion()
+            {
+                if (conexion == null)
+                {
+                    conexion = new SingletonDBConexion();
+                }
+                return conexion;
+            }
+        }
+
+    }
+}
